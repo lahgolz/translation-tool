482a204e6babf9600af2ae0c5cc62920d688efaf
Lahgolz <lahgolz@proton.me>
2026-07-01 22:45:56 +0200

    chore: setup shadcn and tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import adonisjs from '@adonisjs/vite/client';
 import inertia from '@adonisjs/inertia/vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	plugins: [
 		react(),
+		tailwindcss(),
 		inertia({ ssr: { enabled: true, entrypoint: 'inertia/ssr.tsx' } }),
 		adonisjs({ entrypoints: ['inertia/app.tsx'], reload: ['resources/views/**/*.edge'] }),
 	],
